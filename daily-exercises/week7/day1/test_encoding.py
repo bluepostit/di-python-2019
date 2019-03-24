8441e654f7579ca62093ee63f3d86f7815bbae84
bluepostit <32864499+bluepostit@users.noreply.github.com>
2019-03-24 15:15:19 +0200

    add test for consistent encoding

--- daily-exercises/week7/day1/test_encoding.py
+++ daily-exercises/week7/day1/test_encoding.py
@@ -3,6 +3,12 @@ from encoder import Encoder
 
 TEST_MESSAGE = "The quick brown fox jumps over the lazy dog"
 
+def get_code_map(message, code):
+    mapped = {}
+    for (plain_letter, code_letter) in zip(message, code):
+        mapped[plain_letter] = code_letter
+    return mapped
+
 
 def test_constructor():
     e = Encoder()
@@ -23,9 +29,20 @@ def test_decode():
     assert decoded != TEST_MESSAGE
 
 
+def test_consistent_encode():
+    letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ,. ' * 2
+    e = Encoder()
+    encoded = e.encode(letters)
+    mapped = get_code_map(letters, encoded)
+
+    for i in range(len(letters)):
+        plain_letter = letters[i]
+        code_letter = encoded[i]
+        assert code_letter == mapped[plain_letter]
+
+
 def test_encode_and_decode():
     e = Encoder()
     encoded = e.encode(TEST_MESSAGE)
     decoded = e.decode(encoded)
     assert decoded == TEST_MESSAGE
-    
