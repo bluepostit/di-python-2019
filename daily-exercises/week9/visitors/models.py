c71519117b1546c13bd523736a062882981b428b
bluepostit <32864499+bluepostit@users.noreply.github.com>
2019-04-15 15:38:07 +0300

    hotel.8 add models

--- daily-exercises/week9/visitors/models.py
+++ daily-exercises/week9/visitors/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Room(models.Model):
+    number = models.CharField(max_length=50)
+    price = models.FloatField()
+
+
+class Booking(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
